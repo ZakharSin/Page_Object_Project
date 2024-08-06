1a0a9016229ee6556cad6dbc0acc9e07cb4c138d
Zakhar_Sin <zakharsinitsa13@gmail.com>
2024-08-07 00:17:17 +0700

    Mistakes_in_files

--- pages/main_page.py
+++ pages/main_page.py
@@ -1,6 +1,7 @@
-from . base_page import BasePage
-from .locators import MainPageLocators
-from selenium.webdriver.common.by import By
+from pages.base_page import BasePage
+from pages.locators import MainPageLocators
+
+
 class MainPage(BasePage):
     def go_to_login_page(self):
         login_link = self.browser.find_element(*MainPageLocators.LOGIN_LINK)
